b089083408f93f678c155f726087bfaf4258a557
adigeorg <adigeorg@iu.edu>
2025-01-22 20:50:33 -0500

    Added code to main.py

--- exercise_1/main.py
+++ exercise_1/main.py
@@ -1,2 +1,168 @@
-print("Hello World")
-print("Hello Health Informatics")
+# print("Hello World")
+# print("Hello Health Informatics")
+#
+# if 5 > 2:
+#   print("Five is greater than two!")
+
+# This is a comment
+# written in
+# more than just one line
+# print("Hello, World!")
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# x = "awesome"
+#
+# def myfunc():
+#   print("Python is " + x)
+#
+# myfunc()
+
+# x = 20
+# y = str(x)
+# print(type(y))
+
+# b = "Hello, World!"
+# print(b[2:5])
+
+# b = "Hello, World!"
+# print(b[:5])
+
+# b = "Hello, World!"
+# print(b[2:])
+
+# a = "Hello, World!"
+# print(a.upper())
+
+# a = "Hello, World!"
+# print(a.lower())
+
+# a = " Hello, World! "
+# print(a.strip())
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
+
+# a = "Hello, World!"
+# split_value = a.split(",")
+# print(split_value[0])
+
+# a = "Aditi +"
+# b = "George +"
+# c = "INFO-581"
+# d = a + ' ' + b + ' ' + c
+# print(d)
+
+# age = 36
+# name = 'Aditi'
+# txt = f"My name is {name}, I am {age}"
+# print(txt)
+#
+# price = 59
+# txt = f"The price is {price} dollars"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price:.2f} dollars"
+# print(txt)
+
+# txt = f"The price is {20 * 59} dollars"
+# print(txt)
+
+# txt = "hello, and welcome to my world."
+# x = txt.capitalize()
+# print (x)
+
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+#
+# fruit_1 = thislist[1].upper()
+# fruit_2 = thislist[0]
+# fruit_3 = thislist[2].capitalize()
+# sen = f"My favourite fruit is {fruit_1}. I also like {fruit_2}. I may like {fruit_3} "
+# print(sen)
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# thislist[2] = "banana"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+#
+# a = thislist[0].upper()
+# b = thislist[1]
+# c = thislist[2].capitalize()
+# print(f"My favourite fruit is {c}")
+# print(f"My 2nd favourite fruit is {a}")
+# print(f"My favourite drink is {b} milkshake")
+
+# thislist = ["apple", "banana", "cherry"]
+#
+# for x in thislist:
+#   print(x.upper())
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+#
+# list3 = list1 + list2
+# print(list3)
+
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.append("orange")
+
+a = ["apple", "banana", "cherry"]
+b = ["Ford", "BMW", "Volvo"]
+a.append(b)
+
+
+
+
+
+
+
+
